1af02ed788fb15e5b59fc06e21f92926a0e10b27
Jinzhu <wosmvp@gmail.com>
2012-11-21 14:26:22 +0800

    test instance method, class method cached value should be expired after update, destroy

--- test/qor_cache_test.rb
+++ test/qor_cache_test.rb
@@ -14,6 +14,18 @@ class QorCacheTest < ActiveSupport::TestCase
     end
   end
 
+  test "instance method should be updated after update" do
+    product = FactoryGirl.create(:product)
+    method = "slow_method"
+
+    result1 = product.send(method)
+    result2 = product.send(method)
+    product.update_attribute(:updated_at, 1.minute.since)
+    result3 = product.send(method)
+    assert_equal result1, result2
+    assert_not_equal result1, result3
+  end
+
   test "class method should be cached" do
     %w(slow_class_method slow_class_method1).map do |method|
       result1 = Product.send(method)
@@ -22,5 +34,29 @@ class QorCacheTest < ActiveSupport::TestCase
       assert_equal result1, result2
       assert_not_equal result1, result3
     end
+  end
+
+  test "class method should be updated after update" do
+    product = FactoryGirl.create(:product)
+    method = "slow_class_method"
+
+    result1 = Product.send(method)
+    result2 = Product.send(method)
+    product.update_attribute(:updated_at, 1.minute.since)
+    result3 = Product.send(method)
+    assert_equal result1, result2
+    assert_not_equal result1, result3
+  end
+
+  test "class method should be updated after destroy" do
+    product = FactoryGirl.create(:product)
+    method = "slow_class_method"
+
+    result1 = Product.send(method)
+    result2 = Product.send(method)
+    product.destroy
+    result3 = Product.send(method)
+    assert_equal result1, result2
+    assert_not_equal result1, result3
   end
 end
