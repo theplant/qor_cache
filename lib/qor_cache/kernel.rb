e12ee9bc2f4647bc8e6c0151aa66ec8d470dd250
Jinzhu <wosmvp@gmail.com>
2012-11-21 19:44:24 +0800

    qor_cache_key with multi keys and block

--- lib/qor_cache/kernel.rb
+++ lib/qor_cache/kernel.rb
@@ -1,11 +1,15 @@
 require "digest/md5"
 
 module Kernel
-  def qor_cache_key(name)
-    results = Qor::Cache::Configuration.first(:cache_key, name).block.call
-    results = Array(results)
-    results = results.map {|x| x.respond_to?(:cache_key) ? x.cache_key : x.inspect }.join("-")
-    Digest::MD5.hexdigest(results)
+  def qor_cache_key(*names, &blk)
+    objs = names.map { |name| Qor::Cache::Configuration.first(:cache_key, name).block.call }
+    objs << blk.call if block_given?
+
+    results = objs.map do |obj|
+      Array(obj).map {|x| x.respond_to?(:cache_key) ? x.cache_key : x.inspect }.join("-")
+    end
+
+    Digest::MD5.hexdigest(results.join("-"))
   rescue
     puts $!
     rand()
--- test/qor_cache_key_test.rb
+++ test/qor_cache_key_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class QorCacheKeyTest < ActiveSupport::TestCase
-  test "qor_cache_key should be cached, expired correctly" do
+  test "qor_cache_key with one key" do
     cache_key1 = qor_cache_key(:product)
     cache_key2 = qor_cache_key(:product)
     assert_equal cache_key1, cache_key2
@@ -21,5 +21,44 @@ class QorCacheKeyTest < ActiveSupport::TestCase
     product.destroy
     assert_not_equal cache_key3, qor_cache_key(:product)
     assert_not_equal cache_key_pc3, qor_cache_key(:product_and_color_variation)
+  end
+
+  test "qor_cache_key with many keys" do
+    cache_key1 = qor_cache_key(:product, :color_variation)
+    cache_key2 = qor_cache_key(:product, :color_variation)
+    assert_equal cache_key1, cache_key2
+
+    product = FactoryGirl.create(:product)
+
+    cache_key3 = qor_cache_key(:product, :color_variation)
+    assert_not_equal cache_key1, cache_key3
+  end
+
+  test "qor_cache_key with block" do
+    def same_value
+      "same_value"
+    end
+    same_1 = qor_cache_key(:product) do
+      same_value
+    end
+
+    same_2 = qor_cache_key(:product) do
+      same_value
+    end
+
+    assert_equal same_1, same_2
+
+    def random_value
+      rand()
+    end
+    random_1 = qor_cache_key(:product) do
+      random_value
+    end
+
+    random_2 = qor_cache_key(:product) do
+      random_value
+    end
+
+    assert_not_equal random_1, random_2
   end
 end
