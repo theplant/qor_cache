7250b23bc47edb83220962a9590b1e1079b3b211
Jinzhu <wosmvp@gmail.com>
2013-03-28 16:21:32 +0800

    Support timeout for qor_cache_key, raise exception and use rand value when overtime

--- lib/qor_cache/kernel.rb
+++ lib/qor_cache/kernel.rb
@@ -1,16 +1,19 @@
+require 'timeout'
 require "digest/md5"
 require 'uri'
 
 module Kernel
   def qor_cache_key(*names, &blk)
-    objs = names.map { |name| Qor::Cache::Configuration.first(:cache_key, name).block.call }
-    objs << instance_eval(&blk) if block_given?
+    Timeout::timeout(1) do
+      objs = names.map { |name| Qor::Cache::Configuration.first(:cache_key, name).block.call }
+      objs << instance_eval(&blk) if block_given?
 
-    results = objs.flatten.map do |obj|
-      obj.respond_to?(:cache_key) ? obj.cache_key : obj.inspect
+      results = objs.flatten.map do |obj|
+        obj.respond_to?(:cache_key) ? obj.cache_key : obj.inspect
+      end
+
+      Digest::MD5.hexdigest(results.join("-"))
     end
-
-    Digest::MD5.hexdigest(results.join("-"))
   rescue
     puts $!
     rand.to_s
