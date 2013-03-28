3cfbfa086c904372942160bf514f0307e6ea6272
Jinzhu <wosmvp@gmail.com>
2013-03-28 15:38:00 +0800

    use ActiveSupport::Concern to refact code

--- lib/qor_cache/active_record.rb
+++ lib/qor_cache/active_record.rb
@@ -1,55 +1,37 @@
 module Qor
   module Cache
     module ActiveRecord
-      def self.included(base)
-        base.extend SingletonMethods
-        base.send :include, InstanceMethods
-        base.after_save :_qor_cache_expire
-        base.after_destroy :_qor_cache_expire
+      extend ActiveSupport::Concern
 
-        base.before_create :_qor_cache_sync_cached_fields
-        base.after_save :_qor_cache_sync_qor_cache_fields
-      end
-    end
+      included do
+        after_save :_qor_cache_expire
+        after_destroy :_qor_cache_expire
 
-    module SingletonMethods
-      def _qor_cache_key
-        "#{self.name}-cache_key"
+        before_create :_qor_cache_cache_from_external
+        after_save :_qor_cache_cache_to_external
       end
 
-      def cache_key
-        Qor::Cache::Base.cache_store.fetch(_qor_cache_key) do
-          key = self.name.to_s
-          key << Time.now.to_i.to_s
-          key << rand.to_s
-          Digest::SHA1.hexdigest(key)
-        end
-      end
-
-      def _qor_cache_expire
-        Qor::Cache::Base.cache_store.delete(_qor_cache_key)
-        true
-      end
-    end
-
-    module InstanceMethods
+      # After save, expire caches
       def _qor_cache_expire
         self.class._qor_cache_expire
       end
 
-      def _qor_cache_sync_cached_fields
+      # before_create: Cache value from external
+      def _qor_cache_cache_from_external
         nodes = Qor::Cache::Configuration.deep_find(:cache_field) do |node|
           node.parent.is_node?(:scope, self.class.name.demodulize.underscore)
         end
 
         nodes.map do |node|
-          obj = node.options[:from].inject(self) { |obj, value| obj.try(value) }
-          self.send("#{node.name}=", obj)
+          value = node.options[:from].inject(self) { |obj, value| obj = obj.try(value) }
+          self.send("#{node.name}=", value)
         end
       end
 
-      def _qor_cache_sync_qor_cache_fields
+      # after_save: Cache value to external
+      def _qor_cache_cache_to_external
         nodes = Qor::Cache::Configuration.deep_find(:cache_field) do |node|
+          # node.options[:from][-2] -> cache from model
           node.options[:from][-2].to_sym == self.class.name.demodulize.underscore.to_sym
         end
 
@@ -58,9 +40,27 @@ module Qor
           fk = self.class.reflections.select {|name, ref| ref.klass == node_model}.first.last.foreign_key
 
           node_method = node.options[:from][-1]
-          updates = {node.name => try(node_method)}
+          update_attrs = {node.name => try(node_method)}
 
-          node_model.update_all(updates, ["#{fk} = ?", self.id]) if updates.present?
+          node_model.update_all(update_attrs, ["#{fk} = ?", self.id]) if update_attrs.present?
+        end
+      end
+
+      module ClassMethods
+        def _qor_cache_key
+          "#{self.name}-cache_key"
+        end
+
+        def cache_key
+          Qor::Cache::Base.cache_store.fetch(_qor_cache_key) do
+            rand_value = self.name.to_s + rand.to_s
+            Digest::SHA1.hexdigest(rand_value)
+          end
+        end
+
+        def _qor_cache_expire
+          Qor::Cache::Base.cache_store.delete(_qor_cache_key)
+          true
         end
       end
     end
